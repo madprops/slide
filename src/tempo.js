6b699af48f7e1246a0380d8b00adb44017b09bb3
Auric Vente <mprops@protonmail.com>
2025-12-04 12:19:44 -0600

    Mods

--- src/tempo.js
+++ src/tempo.js
@@ -1,7 +1,7 @@
 App.tempo_cpm = App.default_cpm
 App.tempo_storage_key = `slide.tempoCpm`
 App.tempo_debounce_timer = undefined
-App.tempo_commit_delay_ms = 75
+App.tempo_commit_delay_ms = 500
 App.min_tempo = 20
 App.max_tempo = 160
 App.tempo_step = 5
