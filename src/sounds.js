b86cc7583535230369dff45def4e32db1a8478ec
Auric Vente <mprops@protonmail.com>
2025-12-08 16:29:12 -0600

    Mods

--- src/sounds.js
+++ src/sounds.js
@@ -1,4 +1,7 @@
 App.strudel_sounds = [
+  `sawtooth`, `square`, `triangle`, `sine`,
+  `sax`, `gtr`, `moog`,
+  `white`, `pink`, `brown`,
   `bd`, `sd`, `rim`, `hh`, `oh`,
   `lt`, `mt`, `ht`, `rd`, `cr`,
 ]
