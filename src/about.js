d6a6b75a34f935c28045645502cbf00c69f484bc
Auric Vente <mprops@protonmail.com>
2025-12-08 09:54:55 -0600

    Mods

--- src/about.js
+++ src/about.js
@@ -16,7 +16,6 @@ App.create_about_modal = () => {
     <div class="about-item">Clicking the scope on the right produces right panning.</a></div>
     <div class="about-item">Ctrl+s = Starts playback / Updates code.</a></div>
     <div class="about-item">Escape = Closes top modal or stops playback.</a></div>
-    <div class="about-item">Typing bank(" triggers an autocomplete context menu that is filterable.</a></div>
     <div class="about-item">Ctrl+1 = Show the sound context.</a></div>
     <div class="about-item">Ctrl+2 = Show the bank context.</a></div>
     <div class="about-item">Ctrl+3 = Show the note context.</a></div>
