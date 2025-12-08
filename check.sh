aedf14934284e64d874721ed37ec44c422aeb3ce
Auric Vente <mprops@protonmail.com>
2025-12-08 10:17:09 -0600

    Mods

--- check.sh
+++ check.sh
@@ -7,7 +7,7 @@ last_tag=$(git describe --tags --abbrev=0)
 # Pick one
 # files=$(git diff --name-only $last_tag HEAD -- '*.js')
 files=$(git ls-files -- "*.js")
-files=$(echo $files | tr " " "\n" | grep -vE '(^|/)strudel/' | grep -vE '(^|/)songs/' | grep -vE '(^|/)dist/' | grep -vE '(^|/)vite.config.js$' | tr "\n" " ")
+files=$(echo $files | tr " " "\n" | grep -vE '(^|/)strudel/' | grep -vE '(^|/)songs/' | grep -vE '(^|/)dist/' | grep -vE '(^|/)vite.config.js$' | grep -vE '(^|/)src/jdenticon.js' | tr "\n" " ")
 
 if [ -n "$files" ]; then
   npm run --silent lint $files
--- fix.sh
+++ fix.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 files=$(git ls-files -- "*.js")
-files=$(echo $files | tr " " "\n" | grep -vE '(^|/)strudel/' | grep -vE '(^|/)songs/' | grep -vE '(^|/)dist/' | grep -vE '(^|/)vite.config.js$' | tr "\n" " ")
+files=$(echo $files | tr " " "\n" | grep -vE '(^|/)strudel/' | grep -vE '(^|/)songs/' | grep -vE '(^|/)dist/' | grep -vE '(^|/)vite.config.js$' | grep -vE '(^|/)src/jdenticon.js' | tr "\n" " ")
 
 if [ -n "$files" ]; then
   npm run --silent fix $files
