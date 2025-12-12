38de60e57f9fd1d56d02c758798e86c62704a2fb
Auric Vente <mprops@protonmail.com>
2025-12-11 19:41:24 -0600

    Mods

--- songs/finnegan.js
+++ songs/finnegan.js
@@ -10,10 +10,10 @@ let s1 = stack(
 )
 
 let s2 = stack(
-	note("a b c@3 d"),
-	note("g@2 a@0.5 a@0.5"),
+	note("a b c@2 d"),
+	note("g@2 a@0.3 a@0.4"),
 	sound("bd bd [sd hh]").bank("EmuSP12"),
-	sound("hh [hh hh hh]").bank("CasioRZ1").pan(0.9),
+	sound("hh [hh hh hh]").bank("CasioRZ1").pan(0.2),
 )
 
 cat(s1, s2, s1, s2, s1, s2, s1, s2.cpm(2))
