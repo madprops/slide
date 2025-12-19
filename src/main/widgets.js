a1062e39f5dbbe472ba2d56c6b22359f47fc248f
Auric Vente <mprops@protonmail.com>
2025-12-19 15:56:00 -0600

    Mods

--- src/main/widgets.js
+++ src/main/widgets.js
@@ -13,7 +13,7 @@ App.setup_custom_numbers = () => {
     })
 
     DOM.ev(increase, `click`, (event) => {
-let n = event.shiftKey ? 2 : 1
+      let n = event.shiftKey ? 2 : 1
       input.value = parseInt(input.value) + n
       input.dispatchEvent(new Event(`change`))
     })
