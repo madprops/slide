c504f366ea5c35094165edfce6b63bcf5b069722
Auric Vente <mprops@protonmail.com>
2026-01-04 23:53:06 -0600

    Mods

--- server/astro.py
+++ server/astro.py
@@ -657,7 +657,7 @@ class Astro:
 
         name = self.namer.generate_name(ra_avg, dec_avg, mag_avg)
         data.beat_title = name
-        data.beat_code = f"""/*<<<<< o >>>>>
+        data.beat_code = f"""/* <<<<< o >>>>>
 RA: {ra_avg} | DEC: {dec_avg} | MAG: {mag_avg}
 North Star: {self.name(awards["north_star"])}
 Loner Star: {self.name(awards["loner_star"])}
